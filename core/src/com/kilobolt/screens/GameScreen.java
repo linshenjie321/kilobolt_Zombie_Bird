6a4e9ee944228a1759e127b1d7c50bba409eddb8
Shen Jie Lin <linshenjie321@gmail.com>
2014-08-22 19:36:33 -0400

    Zombie Bird day 4 commit, experiemented with drawing box and move it across the screen in x direction

--- core/src/com/kilobolt/screens/GameScreen.java
+++ core/src/com/kilobolt/screens/GameScreen.java
@@ -13,20 +13,15 @@ public class GameScreen implements Screen{
 	
 	public GameScreen() {
 		Gdx.app.log("GameScreen","Attached");
-		
 		world = new GameWorld();
-		renderer = new GameRenderer();
+		renderer = new GameRenderer(world);
 	}
 
 	
 	@Override
 	public void render(float delta) {
-		// Sets a Color to Fill the Screen with (RGB = 10, 15, 230), Opacity of 1 (100%)
-		Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
-		// Fills the screen with the selected color
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		// Covert Frame rate to String, print it
-		Gdx.app.log("GameScreen FPS",(1/delta)+"");
+		world.update(delta);
+		renderer.render();
 		
 	}
 
