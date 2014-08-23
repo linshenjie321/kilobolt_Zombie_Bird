822cb4fa9e1593a10c2f5d3ff69b00185c7086cc
Shen Jie Lin <linshenjie321@gmail.com>
2014-08-23 13:57:11 -0400

    zombie bird day 7 commit, added scrollable, pipe, grass, and declared game objects inside the render for performance

--- core/src/com/kilobolt/gameobjects/Bird.java
+++ core/src/com/kilobolt/gameobjects/Bird.java
@@ -30,11 +30,37 @@ public class Bird {
 		}
 		
 		position.add(velocity.cpy().scl(delta));
+		
+		// Rotate counterclockwise
+        if (velocity.y < 0) {
+            rotation -= 600 * delta;
+
+            if (rotation < -20) {
+                rotation = -20;
+            }
+        }
+
+        // Rotate clockwise
+        if (isFalling()) {
+            rotation += 480 * delta;
+            if (rotation > 90) {
+                rotation = 90;
+            }
+
+        }
 	}
 	
 	public void onClick(){
 		Gdx.app.log("velocity", Float.toString(velocity.y));
 		velocity.y =-140;
+	}
+	
+	public boolean isFalling() {
+	    return velocity.y > 110;
+	}
+
+	public boolean shouldntFlap() {
+	    return velocity.y > 70;
 	}
 	
 	public float getX(){
